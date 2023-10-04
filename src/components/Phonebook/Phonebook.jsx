cef138b1971c4cbd6bba26959f94b0043591809d
MateuszKar2 <system505@protonmail.com>
2023-10-04 18:47:44 +0200

    add file functionsHook

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,37 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import styles from './Phonebook.module.css';
 
 
-class Phonebook extends React.Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const Phonebook = () => {
 
-  componentDidMount() {
+    const [contacts, setContacts] = useState([]);
+    const [filter, setFilter] = useState('');
+
+  useEffect(() =>  {
     const storedContacts = localStorage.getItem('contacts');
     if (storedContacts) {
-      this.setState({contacts: JSON.parse(storedContacts) });
+      setState(JSON.parse(storedContacts));
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-      const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
+
+
+  useEffect(() => {
+    if (contacts.length > 0) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-  }
+  }, [contacts]);
 
-  handleAddContact = newContact => {
-    const { contacts } = this.state;
+  const handleAddContact = newContact => {
     const isDuplicateName = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
@@ -41,23 +35,20 @@ class Phonebook extends React.Component {
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
-  handleDeleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const handleDeleteContact = contactId => {
+    setContacts(prevContacts => 
+      prevContacts.filter(contact => contact.id !== contactId),
+    );
   };
 
-  handleChangeFilter = e => {
-    this.setState({ filter: e.target.value });
+  const handleChangeFilter = e => {
+    this.setFilter(e.target.value);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -65,25 +56,23 @@ class Phonebook extends React.Component {
     );
   };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
+  const filteredContacts = this.getFilteredContacts();
 
-    return (
-      <div>
+return (
+   <div>
         <h1 className={styles.title}>Phonebook</h1>
 
-        <ContactForm onSubmit={this.handleAddContact} />
+        <ContactForm onSubmit={handleAddContact} />
 
         <h2 className={styles.subtitle}>Contacts</h2>
 
-        <Filter value={filter} onChangeFilter={this.handleChangeFilter} />
+        <Filter value={filter} onChangeFilter={handleChangeFilter} />
 
-        <ContactList contacts={filteredContacts} onDeleteContact={this.handleDeleteContact} />
+        <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
       
       </div>
     );
-  }
-}
+
+};
 
 export default Phonebook;
